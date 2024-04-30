3cec9405c2fbce9fec5d4efa12665860906464c9
Santiago Martinez <smartinezc@eurofirms.es>
2024-04-30 12:22:42 +0200

    feat: Get list of products

--- apm/src/app/products/product.service.ts
+++ apm/src/app/products/product.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from './product';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-  // Just enough here for the code to compile
   private productsUrl = 'api/products';
 
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productsUrl);
+  }
 }
