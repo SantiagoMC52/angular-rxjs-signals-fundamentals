5d269fe6c37071938f6fa126466ffb7737421e1f
Santiago Martinez <smartinezc@eurofirms.es>
2024-05-06 14:52:44 +0200

    feat: Use switchMap

--- apm/src/app/products/product.service.ts
+++ apm/src/app/products/product.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of, switchMap, tap } from 'rxjs';
 import { Product } from './product';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { ProductData } from './product-data';
 import { HttpErrorService } from '../utilities/http-error.service';
+import { ReviewService } from '../reviews/review.service';
+import { Review } from '../reviews/review';
 
 @Injectable({
   providedIn: 'root',
@@ -14,18 +15,32 @@ export class ProductService {
   constructor(
     private http: HttpClient,
     private errorService: HttpErrorService,
+    private reviewService: ReviewService,
   ) {}
 
   getProducts(): Observable<Product[]> {
-    return this.http
-      .get<Product[]>(this.productsUrl)
-      .pipe(catchError((err) => this.handleError(err)));
+    return this.http.get<Product[]>(this.productsUrl).pipe(
+      tap(() => console.log('In hhtp.get by id pipeline')),
+      catchError((err) => this.handleError(err)),
+    );
   }
 
   getProduct(id: number): Observable<Product> {
-    return this.http
-      .get<Product>(`${this.productsUrl}/${id}`)
-      .pipe(catchError((err) => this.handleError(err)));
+    return this.http.get<Product>(`${this.productsUrl}/${id}`).pipe(
+      switchMap((product) => this.getProductWithReviews(product)),
+      tap((x) => console.log('x->', x)),
+      catchError((err) => this.handleError(err)),
+    );
+  }
+
+  private getProductWithReviews(product: Product): Observable<Product> {
+    if (product.hasReviews) {
+      return this.http
+        .get<Review[]>(this.reviewService.getReviewUrl(product.id))
+        .pipe(map((reviews) => ({ ...product, reviews }) as Product));
+    } else {
+      return of(product);
+    }
   }
 
   private handleError(err: HttpErrorResponse): Observable<never> {
